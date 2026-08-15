d51d219ad3816e285ebe1c53f94ecedbcd10788e
shdown <shdownnine@gmail.com>
2026-08-16 00:26:11 +0300

    prompt.c: more choices (libedit/no library)

--- prompt.c
+++ prompt.c
@@ -3,8 +3,51 @@
 
 #include "prompt.h"
 
-#include <readline/readline.h>
-#include <readline/history.h>
+#if NO_READLINE
+
+const char *PROMPT_NORMAL = "~~> ";
+const char *PROMPT_CONT = "_...> ";
+
+void prompt_begin(void)
+{
+}
+
+char *prompt_read_line(const char *prompt, bool save)
+{
+    (void) save;
+
+    fputs(prompt, stderr);
+
+    char *buf = NULL;
+    size_t nbuf = 128;
+    ssize_t r = getline(&buf, &nbuf, stdin);
+    if (r > 0) {
+        return buf;
+    } else {
+        fputc('\n', stderr);
+        free(buf);
+        return NULL;
+    }
+}
+
+void prompt_free(char *s)
+{
+    free(s);
+}
+
+void prompt_end(void)
+{
+}
+
+#else
+
+#if READLINE_VIA_LIBEDIT
+# include <editline/readline.h>
+# include <editline/history.h>
+#else
+# include <readline/readline.h>
+# include <readline/history.h>
+#endif
 
 const char *PROMPT_NORMAL = "≈≈> ";
 const char *PROMPT_CONT = "×⋅⋅⋅> ";
@@ -34,3 +77,5 @@ void prompt_free(char *s)
 void prompt_end(void)
 {
 }
+
+#endif
